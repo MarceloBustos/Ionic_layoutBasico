d9a0e300773f535e912953cf4704bbf3c220f7de
Marcelo_Bustos <marcelo.bustoss@gmail.com>
2024-07-11 13:54:39 -0400

    first commit

---
+++ src/views/HomePage.vue
@@ -0,0 +1,46 @@
+<template>
+    <ion-page>
+        <ion-header>
+            <ion-toolbar>
+                <ion-title>Inicio</ion-title>
+            </ion-toolbar>
+        </ion-header>
+        <ion-content class="ion-padding">
+            <p><b>La pelicula de Cars</b> Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam alias,
+                asperiores sapiente corrupti quis harum temporibus fuga sunt! Aspernatur sunt non cupiditate neque fugit
+                repudiandae aperiam reiciendis a dolorum voluptas.</p>
+        </ion-content>
+
+        <ion-footer>
+            <ion-toolbar>
+                <ion-title class="ion-text-center">
+                    @copy Mabo Soft
+                </ion-title>
+            </ion-toolbar>
+        </ion-footer>
+    </ion-page>
+</template>
+
+<script>
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonFooter } from '@ionic/vue';
+
+export default {
+    name: 'Inicio',
+    components: {
+        IonPage,
+        IonHeader,
+        IonToolbar,
+        IonTitle,
+        IonContent,
+        IonFooter
+    }
+}
+</script>
+
+<style>
+p {
+    font-size: 18px;
+    text-align: justify;
+    color: lightseagreen;
+}
+</style>
---
+++ src/views/HomePage2.vue
@@ -0,0 +1,57 @@
+<template>
+  <ion-page>
+    <h1>Grid en Ionic</h1>
+    <ion-grid>
+      <ion-row>
+        <ion-col size="6">
+          <div class="image-container">
+            <img src="/rayo.png" alt="Rayo">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A necessitatibus quibusdam maxime eius sapiente atque nam voluptatum ut non culpa praesentium officia corporis, molestias voluptas reprehenderit laboriosam corrupti, illo maiores!</p>
+          </div>
+        </ion-col>
+        <ion-col size="6">
+          <div class="image-container">
+            <img src="/mate.png" alt="Mate">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora recusandae sit nulla voluptates ab, laborum excepturi consequuntur eum esse! Earum, provident? Amet ratione accusantium non ipsa impedit, a odit ipsum?</p>
+          </div>
+        </ion-col>
+      </ion-row>
+    </ion-grid>
+  </ion-page>
+</template>
+
+<script>
+
+import { IonPage, IonGrid, IonRow, IonCol } from '@ionic/vue';
+
+export default {
+  name: 'HomePage',
+  components: { IonPage, IonGrid, IonRow, IonCol }
+}
+
+</script>
+
+<style>
+
+.image-container{
+  max-width: 100%;
+  max-height: 100%;
+  display: block;
+  margin: auto;
+  border: 1px solid black;
+}
+
+ion-grid{
+  border: 1px solid red;
+  
+}
+
+ion-row {
+  border: 1px solid blue;
+  padding: 50px;
+}
+ion-col {
+  border: 1px solid green;
+  padding: 50px;
+  }
+</style>
